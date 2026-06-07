bf775d804db02b21d9390557e528a222f93800f7
Bakuri Dzagnidze <baku7@MacBook-Air-de-Bakuri.local>
2026-06-07 18:19:31 +0200

    upload components

---
+++ components/footer.php
@@ -0,0 +1,23 @@
+<footer>
+  <div class="footer box">
+    <div class="footer-logo">
+      <a href="#">
+        <img src="./assets/footer logo.png" alt="BON Hotels Footer Logo">
+      </a>
+    </div>
+
+    <div class="footer-nav">
+      <a href="#">Our Hotels</a>
+      <a href="#">Our Conferencing</a>
+      <a href="#">Our Company</a>
+      <a href="#">Hotel Design</a>
+      <a href="#">Careers</a>
+    </div>
+
+    <div class="footer-info">
+      <h3>Bon Hotels Head Office</h3>
+      <a href="tel:+27434344432">+27 434 344 432</a>
+      <a href="mailto:info@bonhotels.com">info@bonhotels.com</a>
+    </div>
+  </div>
+</footer>
